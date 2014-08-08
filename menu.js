cf8eee078c57caf67439898b3cc63bf7515818a8
Matt McFadyen <mmcfadyen91@gmail.com>
2014-08-07 20:11:58 -0400

    :ng: final final final starting point :hamburger:

---
+++ menu.js
@@ -0,0 +1,67 @@
+var menu = [
+	{
+		'burger': 'The American',
+		'price' : 11,
+		'calories' : 420,
+		'fat' : 15,
+		'carbs' : 34,
+		'protein' : 40,
+		'images': [
+			"images/american1.jpg",
+			"images/american2.jpg",
+			"images/american3.jpg"
+		] 
+	},
+	{
+		'burger': 'The Canuck',
+		'price' : 9,
+		'calories' : 666,
+		'fat' : 25,
+		'carbs' : 35,
+		'protein' : 60,
+		'images': [
+			"images/canuck1.jpg",
+			"images/canuck2.jpg",
+			"images/canuck3.jpg"
+		] 
+	},
+	{
+		'burger': 'The Deluxe',
+		'price' : 12,
+		'calories' : 500,
+		'fat' : 25,
+		'carbs' : 55,
+		'protein' : 45,
+		'images': [
+			"images/deluxe1.jpg",
+			"images/deluxe2.jpg",
+			"images/deluxe3.jpg"
+		] 
+	},
+	{
+		'burger': 'The Baconator',
+		'price' : 6,
+		'calories' : 750,
+		'fat' : 40,
+		'carbs' : 60,
+		'protein' : 93,
+		'images': [
+			"images/baconator1.jpg",
+			"images/baconator2.jpg",
+			"images/baconator3.jpg"
+		] 
+	},
+	{
+		'burger': 'The Big Mac',
+		'price' : 5,
+		'calories' : 430,
+		'fat' : 38,
+		'carbs' : 29,
+		'protein' : 21,
+		'images': [
+			"images/bigmac1.jpg",
+			"images/bigmac2.jpg",
+			"images/bigmac3.jpg"
+		] 
+	}
+]
