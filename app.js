f0ddd8c1884b9c23c1fad3173d9a5a8fe739e0bf
Matt McFadyen <mmcfadyen91@gmail.com>
2014-08-07 20:53:03 -0400

    added :hamburger: burger:hamburger: controller

--- app.js
+++ app.js
@@ -3,7 +3,18 @@
 
 	app.controller('menuController',function(){
 		this.menu = menu;
+	});
 
+	app.controller('burgerController',function(){
+		//default image selected from menu.images[] array
+		this.selected = 0;
+
+		//Update the selected image in gallery
+		//@param newSelection - integer referencing the image in menu.images[] array
+		this.setSelected = function(newSelection){
+			//switch the selected image to param
+			this.selected = newSelection;
+		};
 	});
 
 
