9ab6dbf47f3acfb9d7e410eb53b5340ea0665772
Matt McFadyen <mmcfadyen91@gmail.com>
2014-08-08 15:50:56 -0400

    finished click class and image controller

--- app.js
+++ app.js
@@ -3,8 +3,29 @@
 	var app = angular.module('store',[]);
 
 	app.controller('menuController',function(){
+		// assign array of menu items to controllers property 'menu'
 		this.menu = menu;
+	});
+
+	app.controller('imageController',function(){
+		// initializes which image is selected
+		this.selected = 0;
+		
+		this.hidden = false;
+
+		//@param newSelection - integer representing which 
+		//                      image in the images array is being displayed
+		this.setSelected = function(newSelection){
+			this.selected = newSelection;
+		};
+
 	});
 
 
 })();
+
+
+
+
+
+
